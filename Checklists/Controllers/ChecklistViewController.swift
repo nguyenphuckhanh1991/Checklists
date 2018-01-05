a54621e235f7b28e2069529c4695d77e52d7166d
nguyenphuckhanh1991 <khanhnguyen.joy@gmail.com>
2018-01-05 09:40:31 +0700

    01 - Table View

---
+++ Checklists/Controllers/ChecklistViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ChecklistViewController.swift
+//  Checklists
+//
+//  Created by nguyen.phuc.khanh on 1/5/18.
+//  Copyright © 2018 khanh.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+// MARK: UITableViewDataSource
+extension ChecklistViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        return cell
+    }
+}
+// MARK: UITableViewDelegate
+extension ChecklistViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
