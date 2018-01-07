4b9fd52b5302ce3563e94346b927bcee8247dc9d
nguyenphuckhanh1991 <khanhnguyen.joy@gmail.com>
2018-01-07 15:57:44 +0700

    09 - UserDefaults

--- Checklists/Controllers/AllListsViewController.swift
+++ Checklists/Controllers/AllListsViewController.swift
@@ -14,6 +14,19 @@ class AllListsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.delegate = self
+        let index = dataModel.indexOfSelectedChecklist
+        if index >= 0 && index < dataModel.lists.count {
+            let checklist = dataModel.lists[index]
+            performSegue(withIdentifier: "ShowChecklist", sender: checklist)
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     // MARK: Functions
     func makeCell(for tableView: UITableView) -> UITableViewCell {
         let cellIdentifier = "Cell"
@@ -21,7 +34,7 @@ class AllListsViewController: UIViewController {
             tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
             return cell
         } else {
-            return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+            return UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,7 +62,9 @@ extension AllListsViewController: UITableViewDataSource {
         cell.accessoryType = .detailDisclosureButton
         return cell
     }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
         dataModel.lists.remove(at: indexPath.row)
         let indexPaths = [indexPath]
         tableView.deleteRows(at: indexPaths, with: .automatic)
@@ -69,6 +84,7 @@ extension AllListsViewController: UITableViewDataSource {
 // MARK: UITableViewDelegate
 extension AllListsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataModel.indexOfSelectedChecklist = indexPath.row
         let checklist = dataModel.lists[indexPath.row]
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
@@ -93,5 +109,13 @@ extension AllListsViewController: ListDetailViewControllerDelegate {
             }
         }
         dismiss(animated: true, completion: nil)
+    }
+}
+extension AllListsViewController: UINavigationControllerDelegate {
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        // Was the back button tapped?
+        if viewController === self {
+            dataModel.indexOfSelectedChecklist = -1
+        }
     }
 }
--- Checklists/Models/DataModel.swift
+++ Checklists/Models/DataModel.swift
@@ -12,6 +12,17 @@ class DataModel {
     var lists = [Checklist]()
     init() {
         loadChecklists()
+        registerDefaults()
+        handleFirstTime()
+    }
+    var indexOfSelectedChecklist: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "ChecklistIndex")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "ChecklistIndex")
+            UserDefaults.standard.synchronize()
+        }
     }
     func documentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory,
@@ -38,6 +49,22 @@ class DataModel {
             // this line is different from before
             lists = unarchiver.decodeObject(forKey: "Checklists") as! [Checklist]
             unarchiver.finishDecoding()
+        }
+    }
+    func registerDefaults() {
+        let dictionary: [String: Any] = [ "ChecklistIndex": -1,
+                                          "FirstTime": true ]
+        UserDefaults.standard.register(defaults: dictionary)
+    }
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
         }
     }
 }
